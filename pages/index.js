733f8b881dc394a62e4f6e6f301f6c20b9df199c
okunoo <93366478+okunoo@users.noreply.github.com>
2022-11-04 15:41:18 +0900

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,13 @@ export default function Home({ blog }) {
   return (
     <div className={styles.container}>
       <h1>凡庸すぎる日常ブログ</h1>
+      <img src="images/rani.png"alt = "プロフィール画像"></img>
       <h2>これといって何の面白みもない私の日常を綴ったブログサイト</h2>
       <h3>↓以下のテキストをクリックするとブログを見ることができます。↓</h3>
     {blog.map((blog) => (
       <li key={blog.id}>
         <Link href={`blog/${blog.id}`}>
-          <a href="">{blog.title}</a>
+          <a href="" class = "text">{blog.title}</a>
         </Link>
       </li>
     ))}
