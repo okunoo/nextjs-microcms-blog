270851c82d45bd4a4a8c6da41e930a429664ce41
okunoo <93366478+okunoo@users.noreply.github.com>
2022-11-04 15:15:29 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { client } from '../libs/client';
+import styles from '../styles/Home.module.scss'
+//SSG
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" })
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+
+export default function Home({ blog }) {
+  return (
+    <div className={styles.container}>
+      <h1>凡庸すぎる日常ブログ</h1>
+      <h2>これといって何の面白みもない私の日常を綴ったブログサイト</h2>
+    {blog.map((blog) => (
+      <li key={blog.id}>
+        <Link href={`blog/${blog.id}`}>
+          <a href="">{blog.title}</a>
+        </Link>
+      </li>
+    ))}
+  </div>
+  );
+}
